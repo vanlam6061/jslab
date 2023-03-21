208ea9070c888c89d0648736d7a4a362d7dab8c2
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-03-21 17:01:21 +0700

    Add files via upload

---
+++ lab9/script1.js
@@ -0,0 +1,8 @@
+'use strict';
+//Selecting elements
+const player0E1 = document.querySelector('player--0');
+const player0E1 = document.querySelector('player--0');
+const player0E1 = document.querySelector('player--0');
+const player0E1 = document.querySelector('player--0');
+const player0E1 = document.querySelector('player--0');
+const player0E1 = document.querySelector('player--0');
