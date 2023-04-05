58f68761997c6d4604e092b8f4d275e5e8defc27
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-04-05 17:49:51 +0700

    Add files via upload

--- lab13/script.js
+++ lab13/script.js
@@ -1,1 +1,16 @@
 "use strict";
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+const bmw = new Car("Germany", 95);
+bmw.accelerate();
+bmw.brake();
---
+++ lab13/script2.js
@@ -0,0 +1,28 @@
+"use strict";
+class CarCl {
+  constructor(make, speed){
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate(speed){
+    
+}
+
+
+
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
+// const bmw = new Car("Germany", 95);
+// bmw.accelerate();
+// bmw.brake();
