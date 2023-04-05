f20cf3ee2a6760dc4db5eaf127a732df641a6d21
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-04-05 18:04:23 +0700

    Add files via upload

--- lab13/script2.js
+++ lab13/script2.js
@@ -1,16 +1,21 @@
 "use strict";
 class CarCl {
-  constructor(make, speed){
+  constructor(make, speed) {
     this.make = make;
     this.speed = speed;
   }
-  accelerate(speed){
-    
+  accelerate() {
+    this.speed += 10;
+    console.log(this.speed);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(this.speed);
+  }
 }
-
-
-
-
+const bmw = new CarCl("Germany", 95);
+bmw.accelerate();
+bmw.brake();
 // const Car = function (make, speed) {
 //   this.make = make;
 //   this.speed = speed;
